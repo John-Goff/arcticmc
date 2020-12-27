283786fd16d459bba5399fccac48e9f587239456
John Goff <john.g@hiredhippo.ai>
2020-12-26 21:50:20 -0500

    get file name without extension and completion marker

--- lib/paths.ex
+++ lib/paths.ex
@@ -3,8 +3,12 @@ defmodule Arcticmc.Paths do
   Module for accessing the currently defined media paths
   """
 
+  alias Arcticmc.Player
+
   @allowed_keys [:tv, :movies]
   @string_allowed_keys Enum.map(@allowed_keys, &Atom.to_string/1)
+
+  def allowed_paths, do: @allowed_keys
 
   def get(key) when key in @allowed_keys, do: :persistent_term.get({__MODULE__, key})
 
@@ -17,5 +21,38 @@ defmodule Arcticmc.Paths do
       {nil, directory} when is_binary(directory) -> :filename.basedir(:user_config, "arcticmc")
       {nil, nil} -> Path.expand("~/.arcticmc")
     end 
+  end
+
+  def file_name_without_extension(path) do
+    if File.dir?(path) do
+      last_elem_without_checkmark(path)
+    else
+      path
+      |> last_elem_without_checkmark()
+      |> String.split(".")
+      |> Enum.reverse()
+      |> tl()
+      |> Enum.reverse()
+      |> Enum.join(".")
+    end
+  end
+
+  defp last_elem_without_checkmark(path) do
+    path
+    |> Path.split()
+    |> List.last()
+    |> String.replace(Player.played(), "")
+  end
+
+  def parent_directory(directory) do
+    directory
+    |> Path.split()
+    |> Enum.reverse()
+    |> case do
+      [] -> []
+      list -> tl(list)
+    end
+    |> Enum.reverse()
+    |> Path.join()
   end
 end
